97400cedc9d43f0ba5e6aa5590fdaa0f233f5a9b
Nicolas Sarchi <nicolassarchipico@gmail.com>
2023-12-04 17:13:16 -0500

    feat: :sparkles:

---
+++ MultiplicacionRusa/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.WriteLine("Ingrese el valor del multiplicador: ");
+int multiplicador = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese el valor del multiplicando: ");
+int multiplicando = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+while (multiplicador != 0) {
+    if (multiplicador % 2 != 0)
+    {
+        sum += multiplicando;
+    }
+    multiplicador /= 2;
+    multiplicando *= 2;
+}
+
+Console.WriteLine("resultado: " + sum);
